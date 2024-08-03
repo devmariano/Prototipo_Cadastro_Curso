9c2508c3bdf649210994997e41d2fccdf4a92f09
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2024-08-03 20:13:13 -0300

    Separado o mock do componente

---
+++ src/data/mockCourses.js
@@ -0,0 +1,10 @@
+const mockCourses = [
+    { id: 1, name: 'Curso de Elétrica de automóveis', type: 'aperfeicoamento', details: 'Detalhes do curso de Elétrica de automóveis' },
+    { id: 2, name: 'Curso de Elétrica geral', type: 'aperfeicoamento', details: 'Detalhes do curso de Elétrica geral' },
+    { id: 3, name: 'Curso de Elétrica de motos', type: 'especialização', details: 'Detalhes do curso de Elétrica de motos' },
+    { id: 4, name: 'Curso de React', type: 'aperfeicoamento', details: 'Detalhes do curso de React' },
+    { id: 5, name: 'Curso de JavaScript', type: 'iniciacao', details: 'Detalhes do curso de JavaScript' },
+    { id: 6, name: 'Curso de Phyton', type: 'aperfeicoamento', details: 'Detalhes do curso de Phyton' },
+  ];
+  
+  export default mockCourses;
